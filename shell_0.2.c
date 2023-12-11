f34281ac30c0c94b4a68fd3de4de00157580ce35
LAB <lab@kali>
2023-12-11 18:15:46 -0500

    shell_0.3

--- shell_0.2.c
+++ shell_0.2.c
@@ -2,46 +2,56 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include "shell.h"
+#include <sys/wait.h>
 
 #define MAX_CMD_LEN 256
 #define PROMPT "#cisfun$ "
+#define DELIMITERS " \\t\\r\\n\\a"
 
 int main() {
-	char cmd[MAX_CMD_LEN];
-	char *argv[2]; // only the command and NULL
-	argv[1] = NULL;
+    char cmd[MAX_CMD_LEN];
+    char *argv[64];
+    int status;
+    size_t len;
+    int argc; /* Declare argc here */
 
-	while (1) {
-		printf(PROMPT);
-		if (!fgets(cmd, MAX_CMD_LEN, stdin)) {
-			if (feof(stdin)) { // handle EOF
-				printf("\\n");
-				break;
-			} else {
-				perror("fgets");
-				continue;
-			}
-		}
+    while (1) {
+        printf(PROMPT);
 
-		size_t len = strlen(cmd);
-		if (len > 0 && cmd[len - 1] == '\\n') {
-			cmd[len - 1] = '\\0'; // remove newline character
-		}
+        if (!fgets(cmd, MAX_CMD_LEN, stdin)) {
+            if (feof(stdin)) {
+                printf("\\n");
+                break;
+            } else {
+                perror("fgets");
+                continue;
+            }
+        }
 
-		argv[0] = cmd; // set the command
+        len = strlen(cmd);
+        if (len > 0 && cmd[len - 1] == '\n') {
+            cmd[len - 1] = '\0';
+        }
 
-		if (fork() == 0) { // create a child process
-			// Child process: try to execute the command
-			if (execve(cmd, argv, NULL) == -1) {
-				perror(cmd);
-				exit(EXIT_FAILURE);
-			}
-		} else {
-			// Parent process: wait for the child to complete
-			wait(NULL);
-		}
-	}
+        argc = 0; /* Initialize argc here */
+        argv[argc] = strtok(cmd, DELIMITERS);
+        while (argv[argc] != NULL) {
+            argv[++argc] = strtok(NULL, DELIMITERS);
+        }
 
-	return 0;
+        if (argv[0] == NULL) {
+            continue;
+        }
+
+        if (fork() == 0) {
+            if (execvp(argv[0], argv) == -1) {
+                perror(argv[0]);
+                exit(EXIT_FAILURE);
+            }
+        } else {
+            wait(&status);
+        }
+    }
+
+    return 0;
 }
---
+++ shell_0.3.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define MAX_CMD_LEN 256
+#define PROMPT "#cisfun$ "
+#define DELIMITERS " \\t\\r\\n\\a"
+
+char *find_command_in_path(const char *cmd) {
+	char *path = getenv("PATH");
+	char *path_copy = strdup(path);
+	char *token;
+	char *full_cmd;
+
+	for (token = strtok(path_copy, ":"); token != NULL; token = strtok(NULL, ":")) {
+		full_cmd = malloc(strlen(token) + strlen(cmd) + 2);
+		sprintf(full_cmd, "%s/%s", token, cmd);
+
+		if (access(full_cmd, X_OK) == 0) {
+			free(path_copy);
+			return full_cmd; /* Caller must free this */
+		}
+
+		free(full_cmd);
+	}
+
+	free(path_copy);
+	return NULL;
+}
+
+int main() {
+	char cmd[MAX_CMD_LEN];
+	char *argv[64];
+	int status;
+	size_t len;
+	int argc;
+	char *full_cmd_path;
+
+	while (1) {
+		printf(PROMPT);
+
+		if (!fgets(cmd, MAX_CMD_LEN, stdin)) {
+			if (feof(stdin)) {
+				printf("\\n");
+				break;
+			} else {
+				perror("fgets");
+				continue;
+			}
+		}
+
+		len = strlen(cmd);
+		if (len > 0 && cmd[len - 1] == '\n') {
+			cmd[len - 1] = '\0';
+		}
+
+		argc = 0;
+		argv[argc] = strtok(cmd, DELIMITERS);
+		while (argv[argc] != NULL) {
+			argv[++argc] = strtok(NULL, DELIMITERS);
+		}
+
+		if (argv[0] == NULL) {
+			continue;
+		}
+
+		full_cmd_path = find_command_in_path(argv[0]);
+		if (full_cmd_path == NULL) {
+			fprintf(stderr, "%s: command not found\n", argv[0]);
+			continue;
+		}
+
+		if (fork() == 0) {
+			if (execvp(full_cmd_path, argv) == -1) {
+				perror(full_cmd_path);
+				free(full_cmd_path);
+				exit(EXIT_FAILURE);
+			}
+		} else {
+			wait(&status);
+		}
+
+		free(full_cmd_path);
+	}
+
+	return 0;
+}
+
