cc69146308e79956d28f1a07b316498a10b31f88
LAB <lab@kali>
2023-12-13 10:54:08 -0500

    shell_0.2.c

--- shell_0.2.c
+++ shell_0.2.c
@@ -2,26 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
 #include <sys/wait.h>
-#include "shell.h"
+
 #define MAX_CMD_LEN 256
 #define PROMPT "#cisfun$ "
-#define DELIMITERS " \\t\\r\\n\\a"
+#define MAX_ARGS 64
+#define DELIMITERS " \t\r\n\a"
 
 int main() {
     char cmd[MAX_CMD_LEN];
-    char *argv[64];
+    char *argv[MAX_ARGS];
+    pid_t pid;
     int status;
-    size_t len;
-    int argc; /* Declare argc here */
+    size_t len;  /* Declare len at the start of the block */
+    int argc;    /* Declare argc at the start of the block */
 
     while (1) {
         printf(PROMPT);
 
         if (!fgets(cmd, MAX_CMD_LEN, stdin)) {
             if (feof(stdin)) {
-                printf("\\n");
-                break;
+                printf("\n");
+                exit(EXIT_SUCCESS);
             } else {
                 perror("fgets");
                 continue;
@@ -33,23 +36,30 @@ int main() {
             cmd[len - 1] = '\0';
         }
 
-        argc = 0; /* Initialize argc here */
+        argc = 0;
         argv[argc] = strtok(cmd, DELIMITERS);
-        while (argv[argc] != NULL) {
+        while (argv[argc] != NULL && argc < MAX_ARGS - 1) {
             argv[++argc] = strtok(NULL, DELIMITERS);
         }
+        argv[argc] = NULL;
 
         if (argv[0] == NULL) {
             continue;
         }
 
-        if (fork() == 0) {
+        pid = fork();
+        if (pid == -1) {
+            perror("fork");
+            continue;
+        }
+
+        if (pid == 0) {
             if (execvp(argv[0], argv) == -1) {
                 perror(argv[0]);
                 exit(EXIT_FAILURE);
             }
         } else {
-            wait(&status);
+            waitpid(pid, &status, 0);
         }
     }
 
