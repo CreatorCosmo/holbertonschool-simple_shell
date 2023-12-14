d92131abebb0654c46ba21b753c0a742eb0d75ec
LAB <lab@kali>
2023-12-13 19:11:38 -0500

    hsh

--- getline_command.c
+++ getline_command.c
@@ -1,22 +1,35 @@
 #include "shell.h"
-/**
- * _getline_command -  GEts inputs
- * Return: The input.
- */
 
-char *_getline_command(void)
-{
-    char *lineptr = NULL;
-    size_t charter_user = 0;
+char *_getline_command(void) {
+    char *line = NULL;
+    size_t bufsize = 0;
+    ssize_t linelen;
 
-    if (isatty(STDIN_FILENO))
-        write(STDOUT_FILENO, "$ ", 2);
-
-    if (getline(&lineptr, &charter_user, stdin) == -1)
-    {
-        free(lineptr);
-        return (NULL);
+    if (isatty(STDIN_FILENO)) {
+        /* Interactive terminal */
+        /* Implement your interactive input handling here. */
+        /* This could be a simple prompt followed by fgets or a more complex readline implementation. */
+        printf("prompt> "); /* Example prompt */
+        linelen = getline(&line, &bufsize, stdin); /* Using getline as an example */
+    } else {
+        /* Non-interactive input (like from a pipe or file redirection) */
+        linelen = getline(&line, &bufsize, stdin);
     }
 
-    return (lineptr);
+    if (linelen == -1) {
+        if (feof(stdin)) {
+            /* End-of-file reached, handle accordingly */
+            if (line) {
+                free(line);
+            }
+            return NULL;
+        } else {
+            /* Handle getline errors */
+            perror("getline");
+            exit(EXIT_FAILURE); /* Or handle the error as per your requirement */
+        }
+    }
+
+    return line;
 }
+
