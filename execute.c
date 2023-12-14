a71a9c420c3c3c841a67dfcbb1ab4af1a49e8efc
LAB <lab@kali>
2023-12-14 14:47:58 -0500

    hsh

--- execute.c
+++ execute.c
@@ -5,10 +5,11 @@ int shell_cd(char **args);
 int shell_help(char **args);
 int shell_exit(char **args);
 int shell_ctrld(char **args);
+int shell_ls(char **args); /* Prototype for shell_ls */
 
 /* List of built-in command names and their corresponding functions */
-char *builtin_str[] = {"cd", "help", "exit", "^D"};
-int (*builtin_func[]) (char **) = {&shell_cd, &shell_help, &shell_exit, &shell_ctrld};
+char *builtin_str[] = {"cd", "help", "exit", "^D", "ls"};
+int (*builtin_func[]) (char **) = {&shell_cd, &shell_help, &shell_exit, &shell_ctrld, &shell_ls};
 
 /* Returns the number of built-in commands */
 int shell_num_builtins(void)
@@ -41,7 +42,7 @@ int shell_help(char **args)
 	int i;
 	printf("Oscar Bedat and Andres Henderson\n");
 	printf("If you need help, call 1-800-help\n");
-	(void)args;
+	(void)args; /* Args are not used in this function */
 	for (i = 0; i < shell_num_builtins(); i++)
 	{
 		printf("  %s\n", builtin_str[i]);
@@ -53,7 +54,7 @@ int shell_help(char **args)
 /* Exits the shell */
 int shell_exit(char **args)
 {
-	(void)args;
+	(void)args; /* Args are not used in this function */
 	free(args);
 	return (200);
 }
@@ -61,9 +62,17 @@ int shell_exit(char **args)
 /* Handles "^D" input (end-of-transmission) */
 int shell_ctrld(char **args)
 {
-	(void)args;
+	(void)args; /* Args are not used in this function */
 	free(args);
 	return (200);
+}
+
+/* Placeholder for the 'ls' command */
+int shell_ls(char **args)
+{
+	printf("'ls' is not a built-in command in this shell.\n");
+	(void)args; /* Args are not used in this function */
+	return (1);
 }
 
 /* Creates a child process to execute a command */
@@ -74,31 +83,34 @@ int _fork_fun(char **arg, char **av, char **env, char *lineptr, int np, int c)
 	char *format = "%s: %d: %s: not found\n";
 
 	if (arg[0] == NULL)
-		return (1);
+		return (1); /* If no command is provided, return 1 */
+
+	/* Check if the command is a built-in function */
 	for (i = 0; i < shell_num_builtins(); i++)
 	{
 		if (_strcmp(arg[0], builtin_str[i]) == 0)
-			return (builtin_func[i](arg));
+			return (builtin_func[i](arg)); /* Execute the built-in function */
 	}
+
 	child = fork();
-	if (child == 0)
+	if (child == 0) /* Child process */
 	{
-		if (execve(arg[0], arg, env) == -1)
+		if (execve(arg[0], arg, env) == -1) /* Execute the command */
 		{
-			fprintf(stderr, format, av[0], np, arg[0]);
+			fprintf(stderr, format, av[0], np, arg[0]); /* Print error if execve fails */
 			if (!c)
 				free(arg[0]);
 			free(arg);
 			free(lineptr);
-			exit(errno);
+			exit(errno); /* Exit with error code */
 		}
 	}
 	else
 	{
-		wait(&status);
+		wait(&status); /* Parent process waits for the child to finish */
 		if (WIFEXITED(status))
 			return WEXITSTATUS(status); /* Return the exit status of the child process */
 	}
-	return (0);
+	return (0); /* Return 0 on success */
 }
 
--- shell.h
+++ shell.h
@@ -10,12 +10,16 @@
 #include <sys/stat.h>
 #include <errno.h>
 
-
+/* Function prototypes for built-in commands */
 int shell_ctrld(char **args);
 int shell_cd(char **args);
 int shell_help(char **args);
-extern char **environ;
 int shell_exit(char **args);
+int shell_ls(char **args); /* Prototype for shell_ls */
+
+extern char **environ;
+
+/* Utility function prototypes */
 int _strcmp(char *s1, char *s2);
 size_t _strncmp(char *s1, char *s2, size_t n);
 int _strlen(char *s);
@@ -29,8 +33,7 @@ char *_getline_command(void);
 void _getenv(char **env);
 char **tokenize(char *lineptr);
 void _exit_command(char **args, char *lineptr, int _exit);
-int _fork_fun(char **arg, char **av, char **env,
-char *lineptr, int np, int c);
-
+int _fork_fun(char **arg, char **av, char **env, char *lineptr, int np, int c);
 
 #endif /* SHELL_H */
+
