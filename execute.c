b0c508eab957f918d4ff97a15c03420e95709d84
LAB <lab@kali>
2023-12-14 18:06:28 -0500

    hsh

--- execute.c
+++ execute.c
@@ -1,5 +1,6 @@
 #include "shell.h"
-#include <ctype.h>
+#include <ctype.h> /* Include ctype.h for isspace function */
+
 /* Function prototypes for built-in commands */
 int shell_cd(char **args);
 int shell_help(char **args);
@@ -13,34 +14,16 @@ char *builtin_str[] = {"cd", "help", "exit", "^D", "ls"};
 /* Array of function pointers corresponding to the built-in command functions */
 int (*builtin_func[]) (char **) = {&shell_cd, &shell_help, &shell_exit, &shell_ctrld, &shell_ls};
 
-/* shell_num_builtins - Returns the number of built-in commands
- * Return: Number of built-in commands
- */
+/* shell_num_builtins - Returns the number of built-in commands */
 int shell_num_builtins(void)
 {
     return (sizeof(builtin_str) / sizeof(char *));
 }
 
 /* Implementations of built-in functions: */
-/* ... [shell_cd, shell_help, shell_exit, shell_ctrld, shell_ls implementations] ... */
-/* Helper function to trim whitespace from a string */
-char *trim_whitespace(char *str)
-{
-    char *end;
 
-    while(isspace((unsigned char)*str)) str++;
-
-    if(*str == 0)
-        return str;
-
-    end = str + strlen(str) - 1;
-    while(end > str && isspace((unsigned char)*end)) end--;
-
-    end[1] = '\0';
-
-    return str;
-}
-/* shell_cd - Changes the current directory
+/**
+ * shell_cd - Changes the current directory
  * @args: Array of arguments. args[0] is "cd". args[1] is the directory to change to.
  * Return: 1 on success
  */
@@ -60,7 +43,8 @@ int shell_cd(char **args)
     return (1);
 }
 
-/* shell_help - Displays help information for the shell
+/**
+ * shell_help - Displays help information for the shell
  * @args: Array of arguments. Not used in this function.
  * Return: Always returns 1 to continue execution.
  */
@@ -70,7 +54,6 @@ int shell_help(char **args)
 
     printf("Oscar Bedat and Andres Henderson\n");
     printf("If you need help, call 1-800-help\n");
-    /* Args are not used in this function */
     (void)args;
     for (i = 0; i < shell_num_builtins(); i++)
     {
@@ -80,40 +63,57 @@ int shell_help(char **args)
     return (1);
 }
 
-/* shell_exit - Exits the shell
+/**
+ * shell_exit - Exits the shell
  * @args: Array of arguments. Not used in this function.
  * Return: Always returns 0 to terminate execution.
  */
 int shell_exit(char **args)
 {
-    /* Args are not used in this function */
     (void)args;
     free(args);
     return (200);
 }
 
-/* shell_ctrld - Handles "^D" input (end-of-transmission)
+/**
+ * shell_ctrld - Handles "^D" input (end-of-transmission)
  * @args: Array of arguments. Not used in this function.
  * Return: Always returns 0 to terminate execution.
  */
 int shell_ctrld(char **args)
 {
-    /* Args are not used in this function */
     (void)args;
     free(args);
     return (200);
 }
-
-/* shell_ls - Placeholder for the 'ls' command
+/**
+ * shell_ls - Placeholder for the 'ls' command
  * @args: Array of arguments. Not used in this function.
  * Return: Always returns 1 to continue execution.
  */
 int shell_ls(char **args)
 {
     printf("'ls' is not a built-in command in this shell.\n");
-    /* Args are not used in this function */
     (void)args;
     return (1);
+}
+
+/* Helper function to trim whitespace from a string */
+char *trim_whitespace(char *str)
+{
+    char *end;
+
+    while(isspace((unsigned char)*str)) str++;
+
+    if(*str == 0)
+        return str;
+
+    end = str + strlen(str) - 1;
+    while(end > str && isspace((unsigned char)*end)) end--;
+
+    end[1] = '\0';
+
+    return str;
 }
 
 /* Helper function to check if a command is a built-in command */
